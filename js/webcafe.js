963c8772e17952cea6406600e8af0e21e20c1bdb
hyunkkyu <xuangui95@naver.com>
2017-06-29 15:33:25 +0900

    메뉴메뉴 제어를 위한 스크립트 추가

---
+++ js/webcafe.js
@@ -0,0 +1,27 @@
+$(function() {
+    $(".main-menu > li").hover(
+        function() {
+            $(this).children("ul").css("display", "block");
+        },
+        function() {
+            $(this).children("ul").css("display", "none");
+
+
+        });
+
+    $(".main-menu span").focusin(
+        function() {
+            $(this).siblings("ul").css("display", "block");
+        }
+    );
+
+
+    $(".main-menu ul li:last-child a").focusout(
+        function() {
+
+            $(this).parent().parent().css("display", "none");
+
+        }
+
+    );
+});
